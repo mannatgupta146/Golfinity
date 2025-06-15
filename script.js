edfa1e084265630abba454411b56086925f0e46c
mannatgupta146 <mannatgupta146@gmail.com>
2025-06-16 01:50:02 +0530

    added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var crsr = document.querySelector("#cursor");
-var blur = document.querySelector("#cursor-blur");
+var blu = document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove", function(dets) {
     crsr.style.left = dets.x + "px";
     crsr.style.top = dets.y + "px";
-    blur.style.left = dets.x -200 + "px";
-    blur.style.top = dets.y -200 + "px";
+    blu.style.left = dets.x -200 + "px";
+    blu.style.top = dets.y -200 + "px";
 })
 
 
